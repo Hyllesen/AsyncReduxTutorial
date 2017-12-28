eb7cfc1d74947da5d1d8ea1bf7414a3cec659a44
Stefan Hyltoft <stefanhyltoft@gmail.com>
2017-12-28 15:44:53 +0100

    Created Redux actions

---
+++ app/redux/actions/peopleActions.js
@@ -0,0 +1,30 @@
+import {
+  FETCHING_PEOPLE_SUCCESS,
+  FETCHING_PEOPLE_REQUEST,
+  FETCHING_PEOPLE_FAILURE
+} from "./types";
+
+export const fetchingPeopleSuccess = json => ({
+  type: FETCHING_PEOPLE_SUCCESS,
+  payload: json
+});
+
+export const fetchingPeopleRequest = () => ({ type: FETCHING_PEOPLE_REQUEST });
+
+export const fetchingPeopleFailure = error => ({
+  type: FETCHING_PEOPLE_FAILURE,
+  payload: error
+});
+
+export const fetchPeople = () => {
+  return async dispatch => {
+    dispatch(fetchingPeopleRequest());
+    try {
+      let response = await fetch("https://randomuser.me/api/?results=15");
+      let json = await response.json();
+      dispatch(fetchingPeopleSuccess(json));
+    } catch (error) {
+      dispatch(fetchingPeopleFailure(error));
+    }
+  };
+};
