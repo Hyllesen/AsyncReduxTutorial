c6796e3440a9851c350b33601c39330a0bb12b5d
Stefan Hyltoft <stefanhyltoft@Stefans-iMac.local>
2017-12-29 15:26:28 +0100

    Added provider tag with store and middleware

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 import AppContainer from "./app/containers/AppContainer";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import peopleReducer from "./app/redux/reducers/peopleReducer";
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+const store = createStoreWithMiddleware(peopleReducer);
+
 export default class App extends Component {
   render() {
-    return <AppContainer />;
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
   }
 }
---
+++ app/redux/reducers/peopleReducer.js
@@ -0,0 +1,26 @@
+import {
+  FETCHING_PEOPLE_REQUEST,
+  FETCHING_PEOPLE_SUCCESS,
+  FETCHING_PEOPLE_FAILURE
+} from "../actions/types";
+
+const initialState = {
+  isFetching: false,
+  errorMessage: "",
+  people: []
+};
+
+const peopleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_PEOPLE_REQUEST:
+      return { ...state, isFetching: true };
+    case FETCHING_PEOPLE_FAILURE:
+      return { ...state, isFetching: false, errorMessage: action.payload };
+    case FETCHING_PEOPLE_SUCCESS:
+      return { ...state, isFetching: false, people: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default peopleReducer;
